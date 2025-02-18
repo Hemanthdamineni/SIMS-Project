ed7302d2f4552d3f0464b205981b875106fcf50e
HardWriter61467! <142672295+Karthik0000007@users.noreply.github.com>
2025-02-18 14:09:51 +0000

    There are issues once check

--- read_resume.py
+++ read_resume.py
@@ -1,13 +1,15 @@
 import pdfplumber
+import re
 
 def extract_text_from_pdf(pdf_path):
     text = ""
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
             page_text = page.extract_text()
-            if page_text: 
-                text += page_text + "\n"
+            if page_text:
+                page_text = page_text.replace('\n', ' \n')  
+                text += page_text + "\n"  
     return text
 
-resume_text = extract_text_from_pdf(r"Resume.pdf")
-print(resume_text)  
+resume_text = extract_text_from_pdf("Resume.pdf")
+print(resume_text)
