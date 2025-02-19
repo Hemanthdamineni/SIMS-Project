e742385a61912f593f5c80d4ad97be4123630bd9
Hemanthdamineni <144988690+Hemanthdamineni@users.noreply.github.com>
2025-02-19 09:18:09 +0000

    test

--- read_resume.py
+++ read_resume.py
@@ -5,10 +5,8 @@ def extract_text_from_pdf(pdf_path):
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
             page_text = page.extract_text()
-            if page_text:
-                page_text = page_text.replace('\n', ' \n')  
-                text += page_text + "\n"  
+            text += page_text 
     return text
 
-resume_text = extract_text_from_pdf("me.pdf")
+resume_text = extract_text_from_pdf("Resume.pdf")
 print(resume_text)
